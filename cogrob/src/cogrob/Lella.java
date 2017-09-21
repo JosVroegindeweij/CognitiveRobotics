5db683cf21e12769d1172496f6b3669306c6606a
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2017-09-21 09:47:16 +0200

    Lello <3

--- cogrob/src/cogrob/Lella.java
+++ cogrob/src/cogrob/Lella.java
@@ -5,29 +5,27 @@ import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.NXTLightSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.utility.Delay;
-
 public class Lella
 {
-  private static EV3ColorSensor colorSensor=new EV3ColorSensor(SensorPort.S1);
-  private static SensorMode color=colorSensor.getRGBMode();
-  private static float[] colorSample = new float[color.sampleSize()];
-  
+
+  private static EV3ColorSensor colorSensor=new EV3ColorSensor(SensorPort.S2);
+  private static SensorMode color=colorSensor.getAmbientMode();
+  private static float[] colorSample = new float[2];
+  private static NXTLightSensor colorSensor2 = new NXTLightSensor(SensorPort.S1);  
+  private static SensorMode light = colorSensor2.getAmbientMode();
   public static void main(String[] args)
   {
     final EV3 ev3 = (EV3) BrickFinder.getLocal();
     Keys keys = ev3.getKeys();
     
     while (!(keys.getButtons() == Keys.ID_ESCAPE)){
-    System.out.println(lightValue());
-    Delay.msDelay(500);
+      color.fetchSample(colorSample,0);
+      light.fetchSample(colorSample,1);
+      System.out.println(colorSample[0]*100+"  "+ colorSample[1]*100);
+      Delay.msDelay(500);
     }
-  }
-  
-  public static double lightValue()
-  {
-    color.fetchSample(colorSample,0);
-    return (double)((colorSample[0]+colorSample[1]+colorSample[2])*100);
   }
 }
