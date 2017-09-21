0dfe2eeb6519cf00a2d1bee00825dc6a5aec2837
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2017-09-21 10:24:08 +0200

    Agressive behaviour implemented, slow but works, unnecessary prints

--- cogrob/src/cogrob/Lella.java
+++ cogrob/src/cogrob/Lella.java
@@ -3,11 +3,11 @@ package cogrob;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
+import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.NXTLightSensor;
 import lejos.hardware.sensor.SensorMode;
-import lejos.utility.Delay;
 public class Lella
 {
 
@@ -18,15 +18,16 @@ public class Lella
   private static SensorMode light = colorSensor2.getAmbientMode();
   public static void main(String[] args)
   {
-    final boolean off=false;
     final EV3 ev3 = (EV3) BrickFinder.getLocal();
     Keys keys = ev3.getKeys();
     while (!(keys.getButtons() == Keys.ID_ESCAPE)){
-      colorSensor.setFloodlight(off);
       color.fetchSample(colorSample,0);
       light.fetchSample(colorSample,1);
       System.out.println(colorSample[0]*100+"  "+ colorSample[1]*100);
-      Delay.msDelay(500);
+      Motor.A.setSpeed(colorSample[0]*400);
+      Motor.D.setSpeed(colorSample[1]*400-80);
+      Motor.A.forward();
+      Motor.D.forward();
     }
   }
 }
